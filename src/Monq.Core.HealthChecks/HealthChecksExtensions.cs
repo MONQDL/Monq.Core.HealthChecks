b3ec6a6cd6169d0c7ae86a49a71dfbae4dbcde1d
Сергей Письменный <8alig8@gmail.com>
2022-10-26 17:24:14 +0300

    Added RabbitMQCoreClient health check.

---
+++ src/Monq.Core.HealthChecks/HealthChecksExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Monq.Core.HealthChecks.MonqHealthChecks;
+
+namespace Monq.Core.HealthChecks
+{
+    public static class HealthChecksExtensions
+    {
+        public static IHealthChecksBuilder RabbitMQCoreClient(this IHealthChecksBuilder healthChecksBuilder) =>
+            healthChecksBuilder.AddCheck<RabbitMQCoreClientHealthCheck>("RabbitMQ", tags: new[] { "services" });
+    }
+}
