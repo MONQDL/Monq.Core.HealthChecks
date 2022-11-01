35e78534a6d29f71e935404543177d67018eedcd
Сергей Письменный <8alig8@gmail.com>
2022-11-01 14:14:38 +0300

    Added MonqClickHouseCheck.

--- src/Monq.Core.HealthChecks/HealthChecksExtensions.cs
+++ src/Monq.Core.HealthChecks/HealthChecksExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Monq.Core.HealthChecks.MonqHealthChecks;
 using Monq.Core.Redis.Configuration;
 using Monq.Core.Redis.Extentions;
@@ -12,7 +13,8 @@ namespace Monq.Core.HealthChecks
         public static IHealthChecksBuilder AddMonqRabbitMQCheck(this IHealthChecksBuilder healthChecksBuilder) =>
             healthChecksBuilder.AddCheck<RabbitMQCoreClientHealthCheck>("RabbitMQ", tags: new[] { Constants.TagServicesName });
 
-        public static IHealthChecksBuilder AddMonqRedisCheck(this IHealthChecksBuilder healthChecksBuilder, IConfiguration configuration)
+        public static IHealthChecksBuilder AddMonqRedisCheck(this IHealthChecksBuilder healthChecksBuilder,
+            IConfiguration configuration)
         {
             var redisOptions = new RedisOptions();
             configuration.Bind(redisOptions);
@@ -23,5 +25,16 @@ namespace Monq.Core.HealthChecks
         public static IHealthChecksBuilder AddMonqDbContextCheck<TContext>(this IHealthChecksBuilder healthChecksBuilder)
             where TContext : DbContext =>
             healthChecksBuilder.AddDbContextCheck<TContext>(tags: new string[] { Constants.TagServicesName });
+
+        public static IHealthChecksBuilder AddMonqClickHouseCheck(this IHealthChecksBuilder healthChecksBuilder,
+            string connectionString)
+        {
+            return healthChecksBuilder.Add(new HealthCheckRegistration(
+               "ClickHouse",
+               sp => new ClickHouseHealthCheck(connectionString),
+               null,
+               tags: new string[] { Constants.TagServicesName }));
+        }
+
     }
 }
